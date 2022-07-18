00dba25e3fa27d4f27cfb949c1afdb118ef072f9
Muhammad Shafi <shafimalik63@gmail.com>
2022-07-18 18:52:42 +0500

    Update Log File

--- logger/winston.js
+++ logger/winston.js
@@ -1,7 +1,7 @@
 const winston = require("winston");
 const path = require("path");
 require("winston-daily-rotate-file");
-const logsPath = path.join(__dirname, "../../", "servicefinder-logs");
+const logsPath = path.join(__dirname, "../", "servicefinder-logs");
 
 const infoOptions = {
   file: {
