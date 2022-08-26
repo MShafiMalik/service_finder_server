c8b4df3ae02560fd53d09e53a6ab5f1a0f494be3
Muhammad Shafi <shafimalik63@gmail.com>
2022-08-26 14:55:34 +0500

    Config Change

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   PORT: process.env.PORT,
-  // DB_URL: process.env.DB_URL,
-  DB_URL: "mongodb://127.0.0.1:27017/service_finder",
-  // SERVER_BASE_URL: process.env.SERVER_BASE_URL,
-  SERVER_BASE_URL: "http://localhost:5000",
+  DB_URL: process.env.DB_URL,
+  // DB_URL: "mongodb://127.0.0.1:27017/service_finder",
+  SERVER_BASE_URL: process.env.SERVER_BASE_URL,
+  // SERVER_BASE_URL: "http://localhost:5000",
   DOMAIN_BASE_URL: process.env.DOMAIN_BASE_URL,
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
   URL_CRYPT_SECRET_KEY: process.env.URL_CRYPT_SECRET_KEY,
